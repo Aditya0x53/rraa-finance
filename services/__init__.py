b48db68d95178430cffee29e0d4f415621212f46
Aditya0x53 <adilnpgnt@gmail.com>
2026-06-06 11:17:12 +0530

    Add services __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,15 @@
+from .customer_service import CustomerService
+from .loan_service import LoanService
+from .collection_service import CollectionService
+from .penalty_service import PenaltyService
+from .report_service import ReportService
+from .audit_service import AuditService
+
+__all__ = [
+    "CustomerService",
+    "LoanService",
+    "CollectionService",
+    "PenaltyService",
+    "ReportService",
+    "AuditService",
+]
