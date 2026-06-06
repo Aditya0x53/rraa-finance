e5b69576cf4693b808c4a863f0a03a62b44c77d5
Aditya0x53 <adilnpgnt@gmail.com>
2026-06-06 11:14:57 +0530

    Add models __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+from .customer import Customer
+from .loan import Loan
+from .collection import Collection
+from .penalty import Penalty
+from .user import User
+from .audit_log import AuditLog
+
+__all__ = [
+    "Customer",
+    "Loan",
+    "Collection",
+    "Penalty",
+    "User",
+    "AuditLog"
+]
